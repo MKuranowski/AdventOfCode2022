3f4a57adb8e0ccad41a2ceb5c595b598db3d705f
Mikołaj Kuranowski <mkuranowski@gmail.com>
2022-12-10 16:41:03 +0900

    Day 10

---
+++ src/day10a.ts
@@ -0,0 +1,57 @@
+import { linesFromFile } from "./core.ts";
+
+export class Processor {
+    x = 1;
+    cycle = 1;
+
+    before_cycle_callback: null | ((p: Processor) => void) = null;
+    after_cycle_callback: null | ((p: Processor) => void) = null;
+
+    private startCycle(): void {
+        if (this.before_cycle_callback !== null) this.before_cycle_callback(this);
+    }
+
+    private endCycle(): void {
+        ++this.cycle;
+        if (this.after_cycle_callback !== null) this.after_cycle_callback(this);
+    }
+
+    execute(instruction_row: string): void {
+        const instruction = instruction_row.split(" ");
+        switch (instruction[0]) {
+            case "noop":
+                this.startCycle();
+                this.endCycle();
+                break;
+
+            case "addx": {
+                const delta = parseInt(instruction[1], 10);
+
+                // First cycle is empty
+                this.startCycle();
+                this.endCycle();
+
+                // Second cycle actually adds
+                this.startCycle();
+                this.x += delta;
+                this.endCycle();
+
+                break;
+            }
+        }
+    }
+}
+
+export function main(): void {
+    let sum = 0;
+    const cpu = new Processor();
+
+    cpu.before_cycle_callback = (cpu) => {
+        if (cpu.cycle % 40 === 20) sum += cpu.cycle * cpu.x;
+    };
+
+    for (const i of linesFromFile(Deno.args[0]))
+        cpu.execute(i);
+
+    console.log(sum);
+}
---
+++ src/day10b.ts
@@ -0,0 +1,37 @@
+import { linesFromFile } from "./core.ts";
+import { Processor } from "./day10a.ts";
+
+export class CRT {
+    picture: string[][];
+
+    constructor() {
+        this.picture = [];
+        for (let i = 0; i < 6; ++i) this.picture.push(new Array(40).fill("."));
+    }
+
+    print() {
+        console.log(this.picture.map(row => row.join("")).join("\n"));
+    }
+
+    processor_cb(cpu: Processor): void {
+        const pixelIdx = (cpu.cycle - 1) % 240;
+        const row = (pixelIdx / 40) >> 0;  // integer division
+        const col = pixelIdx % 40;
+
+        if (col === cpu.x || col - 1 === cpu.x || col + 1 === cpu.x)
+            this.picture[row][col] = "#";
+    }
+}
+
+
+export function main(): void {
+    const cpu = new Processor();
+    const crt = new CRT();
+
+    cpu.before_cycle_callback = crt.processor_cb.bind(crt);
+
+    for (const i of linesFromFile(Deno.args[0]))
+        cpu.execute(i);
+
+    crt.print();
+}
