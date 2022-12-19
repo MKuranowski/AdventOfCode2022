32ceba13f308c2d86e20cba2103279431767213e
Mikołaj Kuranowski <mkuranowski@gmail.com>
2022-12-19 16:27:00 +0900

    Remove pointless read permissions for day16

--- aoc.sh
+++ aoc.sh
@@ -14,10 +14,10 @@ fi
 # Day 16 is special
 if [ "$DAY" = "16a" ]; then
     rustc src/day16.rs -O -o ./src/day16
-    deno run --allow-read src/day16_graph_compression.ts <"$FILENAME" | ./src/day16 a
+    deno run src/day16_graph_compression.ts <"$FILENAME" | ./src/day16 a
 elif [ "$DAY" = "16b" ]; then
     rustc src/day16.rs -O -o ./src/day16
-    deno run --allow-read src/day16_graph_compression.ts <"$FILENAME" | ./src/day16 b
+    deno run src/day16_graph_compression.ts <"$FILENAME" | ./src/day16 b
 else
     echo "import { main } from \"./src/day$DAY.ts\"; main();" | deno run --allow-read=input - "$FILENAME"
 fi
